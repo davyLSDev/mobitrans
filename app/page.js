7f78a22aa28c0142c03d602897edf8b1eba7cfdf
Davy Tenn <davy.lsdev@gmail.com>
2024-11-21 21:20:13 -0700

    Create a web app with toolbar and button

---
+++ app/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import Toolbar from './components/Toolbar';
+import TextGrid from './components/TextGrid';
+
+export default function Home() {
+  return (
+    <main>
+      <Toolbar />
+      <TextGrid />
+    </main>
+  );
+} 
